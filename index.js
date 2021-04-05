835d6adee25214d7e4992d06a7fee1aca9e52189
Morgan Kemper <morgankemper@Morgans-Air.attlocal.net>
2021-04-05 18:24:11 -0500

    cors update

--- index.js
+++ index.js
@@ -41,8 +41,22 @@ require('./passport');
 
 let auth = require('./auth')(app);
 
-// const cors = require('cors');
+const cors = require('cors');
 // app.use(cors());
+let allowedOrigins = ["http://localhost:8080", "http://localhost:1234", "https://findamovieflix.herokuapp.com", "http://localhost:4200"];
+
+
+// app.use(cors()); //By default, this will allow all domains to make requests to the API. The commented code below restricts this to specific origins.
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}))
 
 const { check, validationResult } = require('express-validator');
 
