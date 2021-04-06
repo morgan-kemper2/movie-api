083d3e64ae29b9c91331d2d920641beac63fd5ce
Morgan Kemper <morgankemper@Morgans-Air.attlocal.net>
2021-04-05 22:27:56 -0500

    cors

--- index.js
+++ index.js
@@ -43,20 +43,15 @@ let auth = require('./auth')(app);
 
 const cors = require('cors');
 // app.use(cors());
-let allowedOrigins = ["http://localhost:8080", "http://localhost:1234", "https://findamovieflix.herokuapp.com", "http://localhost:4200","https://morgan-kemper2.github.io/myflix-angular-client2/welcome"];
+// let allowedOrigins = ["http://localhost:8080", "http://localhost:1234", "https://findamovieflix.herokuapp.com", "http://localhost:4200","https://morgan-kemper2.github.io/myflix-angular-client2/welcome"];
 
 
 // app.use(cors()); //By default, this will allow all domains to make requests to the API. The commented code below restricts this to specific origins.
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}))
+app.use(function (request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const { check, validationResult } = require('express-validator');
 
